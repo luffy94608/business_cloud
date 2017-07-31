0ed3fa93a20014381fc294000aac6d0b467a26c0
luffy <29620639@qq.com>
2017-07-31 19:36:48 +0800

    添加市场数据分析和企业数据分析静态页面

--- resources/views/index/business.blade.php
+++ resources/views/index/business.blade.php
@@ -1,30 +1,41 @@
-<script src='http://api.map.baidu.com/api?v=2.0&ak={{Config::get('app')['ak']}}' type='text/javascript'></script>
-<script type="text/javascript" src="http://developer.baidu.com/map/jsdemo/demo/convertor.js"></script>
+@extends('layouts.default')
+@section('title', '市场数据分析')
 
-<div class="dis-animate-overlay" id="ap_search_modal">
-    <header class="header">
-        <div class="header-left">
-            <i class="header-icon icon-v-left js_back " onclick="this.parentNode.parentNode.parentNode.className = 'dis-animate-overlay'"></i>
-        </div>
-        <div class="header-right">
-        </div>
-        <h1 class="page-tt">搜索地址</h1>
-    </header>
-    <main id="pages" class="page-wrap">
-        <div class="page">
-            <div class="ap-search-section display-flex">
-                <div class="p-10">
-                    <svg class="icon-svg fill-grey fxWH-25">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/icons.svg#icon-search"></use>
-                    </svg>
+{{--内容区域--}}
+@section('content')
+    {{--header--}}
+    @include('layouts.header')
+    {{--banner--}}
+    @include('templates.banner')
+
+    {{--content--}}
+    <div class="container-fluid">
+        <div class="container ">
+            <div class="row  mt-100 pb-100  bg-white box-shadow-1 bc-form-section">
+                <div class="bc-section-title color-with-body">市场数据分析</div>
+                <div class="col-xs-12 col-sm-10 col-sm-offset-1 form-horizontal mt-100">
+
+                    <div class="form-group">
+                        <label for="js_input_company" class="col-sm-2 col-xs-3 control-label">关注企业名称</label>
+                        <div class="col-sm-4 col-xs-7">
+                            <input type="email" class="form-control" id="js_input_company" placeholder="请输入企业名称">
+                        </div>
+                    </div>
+
+                    {{--提交按钮--}}
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-success">提交</button>
+                        </div>
+                    </div>
                 </div>
-                <input class="box-flex-1" type="text" id="search_input" placeholder="请输入搜索地址">
-            </div>
-            <ul class="ap-order-list search gone" id="search_result_panel">
-            </ul>
-            <ul class="ap-order-list search " id="search_history_panel">
 
-            </ul>
+            </div>
         </div>
-    </main>
-</div>
+        @include('templates.ad')
+    </div>
+
+    @include('layouts.footer', ['style'=>''])
+@stop
+
+
--- resources/views/index/company.blade.php
+++ resources/views/index/company.blade.php
@@ -1,30 +1,80 @@
-<script src='http://api.map.baidu.com/api?v=2.0&ak={{Config::get('app')['ak']}}' type='text/javascript'></script>
-<script type="text/javascript" src="http://developer.baidu.com/map/jsdemo/demo/convertor.js"></script>
+@extends('layouts.default')
+@section('title', '企业数据分析')
 
-<div class="dis-animate-overlay" id="ap_search_modal">
-    <header class="header">
-        <div class="header-left">
-            <i class="header-icon icon-v-left js_back " onclick="this.parentNode.parentNode.parentNode.className = 'dis-animate-overlay'"></i>
-        </div>
-        <div class="header-right">
-        </div>
-        <h1 class="page-tt">搜索地址</h1>
-    </header>
-    <main id="pages" class="page-wrap">
-        <div class="page">
-            <div class="ap-search-section display-flex">
-                <div class="p-10">
-                    <svg class="icon-svg fill-grey fxWH-25">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/icons.svg#icon-search"></use>
-                    </svg>
+{{--内容区域--}}
+@section('content')
+    {{--header--}}
+    @include('layouts.header')
+    {{--banner--}}
+    @include('templates.banner')
+    
+    {{--content--}}
+    <div class="container-fluid">
+        <div class="container ">
+            <div class="row  mt-100 pb-100  bg-white box-shadow-1 bc-form-section">
+                <div class="bc-section-title color-with-body">企业数据分析</div>
+                <div class="col-xs-12 col-sm-10 col-sm-offset-1 form-horizontal mt-100">
+
+                    <div class="form-group">
+                        <label for="js_input_area" class="col-sm-2 col-xs-3 control-label">关注地区</label>
+                        <div class="col-sm-4 col-xs-7">
+                            <select id="js_input_area" class="form-control">
+                                <option>北京市</option>
+                                <option>上海市</option>
+                                <option>重庆市</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="js_input_industry_care" class="col-sm-2 col-xs-3 control-label">关注行业</label>
+                        <div class="col-sm-4 col-xs-7">
+                            <select id="js_input_industry_care" class="form-control">
+                                <option>计算机</option>
+                                <option>游戏</option>
+                                <option>音乐</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="js_input_industry_care" class="col-sm-2 col-xs-3 control-label">关注时间</label>
+                        <div class="col-sm-4 col-xs-7">
+                            <select id="js_input_industry_care" class="form-control">
+                                <option>最近一月</option>
+                                <option>最近两月</option>
+                                <option>最近三月</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="js_input_mobile" class="col-sm-2 col-xs-3 control-label">关注关键词</label>
+                        <div class="col-sm-4 col-xs-7">
+                            <div class="bc-keyword-section">
+                                <span class="bck-item">房屋建筑</span>
+                                <span class="bck-item active">基础建筑</span>
+                                <span class="bck-item">水利建筑</span>
+                                <span class="bck-item">工程建筑</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    {{--提交按钮--}}
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-success">提交</button>
+                        </div>
+                    </div>
                 </div>
-                <input class="box-flex-1" type="text" id="search_input" placeholder="请输入搜索地址">
+                
             </div>
-            <ul class="ap-order-list search gone" id="search_result_panel">
-            </ul>
-            <ul class="ap-order-list search " id="search_history_panel">
-
-            </ul>
         </div>
-    </main>
-</div>
+        @include('templates.ad')
+    </div>
+
+    @include('layouts.footer', ['style'=>''])
+@stop
+
+
