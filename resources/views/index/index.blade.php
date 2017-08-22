4275256a2b464a92ed39640dc6a9591e83d9492b
luffy <29620639@qq.com>
2017-08-22 19:06:46 +0800

    修改header 颜色

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -44,7 +44,7 @@
                                     <p class="title">竞争对手</p>
                                     <p class="total color-purple">{{ $data['competitor_today'] }}</p>
                                     <div class="pl-30 pr-30 ">
-                                        <p class="hint text-left">今日新增<span class="color-purple">{{ $data['competitor_today'] }}</span>条数据</p>
+                                        <p class="hint">今日新增<span class="color-purple">{{ $data['competitor_today'] }}</span>条数据</p>
                                         <div class="progress bid-progress mt-20">
                                             <div class="progress-bar bg-purple" role="progressbar" data-percent="{{$data['competitor_percent']}}%" aria-valuemin="0" aria-valuemax="100" ></div>
                                         </div>
